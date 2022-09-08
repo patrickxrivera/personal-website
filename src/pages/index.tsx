d7fe4c08bfeb4524df314472df64a5fe0614721b
Patrick Rivera <patrick.x.rivera@gmail.com>
2022-09-08 13:44:01 -0400

    feat: more finesse

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
 import { withLayout } from '@moxy/next-layout'
-import { DocumentDuplicateIcon, PencilSquareIcon, CheckIcon } from '@heroicons/react/24/outline'
+import {
+  DocumentDuplicateIcon,
+  PencilSquareIcon,
+  CheckIcon,
+  ArrowUpRightIcon,
+} from '@heroicons/react/24/outline'
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Text from '../components/Text'
@@ -18,41 +23,30 @@ type Post = {
 
 const posts: Post[] = [
   {
-    title: 'Crypto Fundamentals and NFTs',
+    title: 'Web3 Fundamentals',
     description:
       'Presentation for the On Deck community that covers: eras of the web, blockchain architecture, last 10 years of crypto, rise of NFTs, and the future of NFTs',
     date: 'March 12th, 2021',
     url: 'https://p.mirror.xyz',
   },
   {
-    title: 'Come for the Creator, Stay for the Economy',
-    description: 'Lorem ipsum solar deleris. Lorem ipsum solar deleris.',
+    title: 'Web3 and the Creator Economy',
+    description: `Rethinking the classic strategy "Come for the tool, stay for the network" in the context of web3 and cryptonetworks`,
     date: 'April 20th, 2021',
-    url: 'https://p.mirror.xyz',
+    url: 'https://p.mirror.xyz/1EpvJwUpx_KlRcHOKLNqADEkwHL_Z1ZYKobF2uLwgBg',
   },
   {
     title: 'Thoughts on DAO Tooling',
-    description: 'Lorem ipsum solar deleris. Lorem ipsum solar deleris.',
-    date: 'April 20th, 2021',
-    url: 'https://p.mirror.xyz',
+    description: `Building for DAOs today is a bit tricky so I shared some lessons from building for DAOs at Mirror and as a participant in many of these communities`,
+    date: 'March 23rd, 2022',
+    url: 'https://p.mirror.xyz/VlaQ_fH2NNXh0MNyhj4CI13wX8zwJ6boctVpagApPLc',
   },
   {
     title: 'A Guide to Crypto Tokens',
-    description: 'Lorem ipsum solar deleris. Lorem ipsum solar deleris.',
-    date: 'April 20th, 2021',
-    url: 'https://p.mirror.xyz',
-  },
-  {
-    title: 'Community DAOs',
-    description: 'Lorem ipsum solar deleris. Lorem ipsum solar deleris.',
-    date: 'April 20th, 2021',
-    url: 'https://p.mirror.xyz',
-  },
-  {
-    title: 'Tips for Creators Getting Into Crypto',
-    description: 'Lorem ipsum solar deleris. Lorem ipsum solar deleris.',
-    date: 'April 20th, 2021',
-    url: 'https://p.mirror.xyz',
+    description:
+      'Deep dive into fungible tokens / NFTs and how they can be used in virtual economies - published in A16Z Future',
+    date: 'May 23rd, 2021',
+    url: 'https://future.com/a-taxonomy-of-tokens-distinctions-with-a-difference/',
   },
 ]
 
@@ -74,7 +68,7 @@ const timeline: TimelineItem[] = [
     url: 'https://mirror.xyz',
     date: 'Mar 2021 - Sep 2022 · 1 yr 7 mos',
     description:
-      'Cras eget hendrerit purus. Maecenas at tellus mauris. Phasellus sodales facilisis pellentesque. Duis venenatis, enim non volutpat bibendum, augue nisl commodo ligula, ac mattis dolor nibh sit amet ipsum. Fusce nisi enim, congue ut gravida vel, imperdiet a diam. Sed sollicitudin risus vitae dolor rhoncus, in vehicula velit pharetra. Proin suscipit rhoncus diam quis rutrum. ',
+      'Home for web3 publishing. Backed by A16Z Crypto, USV, Variant Fund, Balaji, Naval, and others. I was employee #3.\n\nWrote product specs, built full-stack features, integrated smart contracts, and worked closely with creators and community builders to harness the power of web3.',
     logoPath: '/mirror-logo.jpeg',
   },
   {
@@ -84,7 +78,7 @@ const timeline: TimelineItem[] = [
     url: 'https://techcrunch.com/2022/01/18/opensea-buys-defi-wallet-startup-dharma-labs-appoints-new-cto/',
     date: 'Jan 2020 - Oct 2020 · 10 mos',
     description:
-      'Cras eget hendrerit purus. Maecenas at tellus mauris. Phasellus sodales facilisis pellentesque. Duis venenatis, enim non volutpat bibendum, augue nisl commodo ligula, ac mattis dolor nibh sit amet ipsum. Fusce nisi enim, congue ut gravida vel, imperdiet a diam. Sed sollicitudin risus vitae dolor rhoncus, in vehicula velit pharetra. Proin suscipit rhoncus diam quis rutrum. ',
+      'Acquired by OpenSea. Led engineering projects across backend, UI, growth, and analytics. Left to pursue startup ideas.',
     logoPath: '/dharma-logo.jpeg',
   },
   {
@@ -94,7 +88,7 @@ const timeline: TimelineItem[] = [
     url: 'https://instacart.com',
     date: 'Nov 2018 - Jan 2020 · 1 yr 3 mos',
     description:
-      'Cras eget hendrerit purus. Maecenas at tellus mauris. Phasellus sodales facilisis pellentesque. Duis venenatis, enim non volutpat bibendum, augue nisl commodo ligula, ac mattis dolor nibh sit amet ipsum. Fusce nisi enim, congue ut gravida vel, imperdiet a diam. Sed sollicitudin risus vitae dolor rhoncus, in vehicula velit pharetra. Proin suscipit rhoncus diam quis rutrum. ',
+      'Full-stack engineer on the Growth team building features to engage and retain customers.',
     logoPath: '/instacart-logo.jpeg',
   },
 ]
@@ -292,7 +286,10 @@ function Home() {
           {posts.map(({ title, description, date, url }) => (
             <div className="w-92 mt-6" key={`${url}-${title}`}>
               <Link url={url} openInNewTab>
-                <h4 className="font-bold text-lg lg:text-base">{title}</h4>
+                <div className="flex items-center">
+                  <h4 className="font-bold text-lg lg:text-base">{title}</h4>
+                  <ArrowUpRightIcon className="ml-2" width={18} height={18} strokeWidth={2} />
+                </div>
                 <p className="mt-2 text-lg lg:text-sm">{description}</p>
                 <p className="mt-2 text-lg lg:text-sm text-gray-subheading">{date}</p>
               </Link>
